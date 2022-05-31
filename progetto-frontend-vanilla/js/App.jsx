c95a1bd924e2808133eaa4279933d59c14adde26
Simone De Blasi <s.deblasi@itsrizzoli.it>
2022-05-31 13:25:13 +0200

    fix: changes

---
+++ progetto-frontend-vanilla/js/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Button} from "simone-react/atoms/button/Button"
+
+const App = () => ( 
+    <div>
+        <h1>Hello React</h1>
+        <Button label={"gasssss"} />
+    </div>
+);
+
+export default App;
